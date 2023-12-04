32b7828c03670d51996a72476c45ebc159e25af3
h4ppyturt1e <ryan170402@gmail.com>
2023-12-03 17:06:01 -0800

    added template for each day

---
+++ Day 13/day13.py
@@ -0,0 +1,11 @@
+
+def solve(data):
+    return
+
+
+if __name__ == '__main__':
+    # read test file
+    with open('test.txt', 'r') as f:
+        test = f.read()
+    
+    solve(test)
---
+++ createFiles.py
@@ -0,0 +1,27 @@
+# create python files and according test files for each day in the advent of code
+
+import os
+import shutil
+
+def createFiles():
+    print('Creating files...')
+    for i in range(1,26):
+        # create folder
+        foldername = 'Day ' + str(i)
+        if not os.path.exists(foldername):
+            os.makedirs(foldername)
+        
+        # create python file
+        filename = os.path.join(foldername, 'day' + str(i) + '.py')
+        if not os.path.exists(filename):
+            shutil.copyfile('template.py', filename)
+        
+        # create test file
+        filename = os.path.join(foldername, 'test' + str(i) + '.txt')
+        if not os.path.exists(filename):
+            with open(filename, 'w') as f:
+                f.write('')
+    return
+
+if __name__ == '__main__':
+    createFiles()
