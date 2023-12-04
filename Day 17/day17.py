df2f6dc67ec522d44ca037f9f1e7b2988639a75a
h4ppyturt1e <ryan170402@gmail.com>
2023-12-03 17:32:29 -0800

    added time elapsed for templates

--- Day 17/day17.py
+++ Day 17/day17.py
@@ -1,11 +1,25 @@
+from time import time
 
-def solve(data):
+
+def solvePart1(data):
     return
 
+def solvePart2(data):
+    return
 
 if __name__ == '__main__':
     # read test file
     with open('test.txt', 'r') as f:
         test = f.read()
     
-    solve(test)
+    start = time()
+    
+    part1 = solvePart1(test)
+    p1Time = time() - start
+    
+    part2 = solvePart2(test)
+    p2Time = time() - p1Time
+    
+    # print solutions and show time elapsed
+    print(f'Part 1 ({p1Time:.4f}s):\n{part1}\n')
+    print(f'Part 2 ({p2Time:.4f}s):\n{part2}\n')
---
+++ getTest.py
@@ -0,0 +1,33 @@
+import sys
+import requests
+
+# login to adventofcode.com in your browser
+# download file from https://adventofcode.com/2023/day/X/input
+# call it by python getTest.py X
+# write it to ./Day X/testX.txt
+
+def download_input(day):
+    # read session cookie from session_cookie.txt    
+    with open("session_cookie.txt", "r") as file:
+        session_cookie = file.read().strip()
+    
+    url = f"https://adventofcode.com/2023/day/{day}/input"
+    headers = {"Cookie": f"session={session_cookie}"}
+    
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    input_text = response.text.strip()
+    file_path = f"./Day {day}/test{day}.txt"
+    
+    with open(file_path, "w") as file:
+        file.write(input_text)
+    
+    print(f"Input file for Day {day} downloaded successfully.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python getTest.py <day>")
+        sys.exit(1)
+    
+    day = sys.argv[1]
+    download_input(day)
