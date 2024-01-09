c1efbe7924470961993273191b127733de53453e
h4ppyturt1e <ryan170402@gmail.com>
2024-01-08 16:31:04 -0800

    finished day 4. this was fun

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -1,11 +1,39 @@
 from time import time
-
+import re
+from math import floor
 
 def solvePart1(data):
-    return
+    data = [line.split(": ")[1].split(" | ") for line in data.split("\n")]
+    
+    lineData = []
+    
+    for true, owned in data:
+        true = {int(x) for x in true.split(" ") if x}
+        owned = {int(x) for x in owned.split(" ") if x}
+        lineData.append((true, owned))
+    
+    # print(lineData)
+    
+    totalPoints = 0
+    countList = []
+    for true, owned in lineData:
+        count = sum([1 for x in owned if x in true])
+        points = floor(2 ** (count - 1))
+        # print(points)
+        totalPoints += points
+        countList.append(count)
+        
+    return totalPoints, countList
 
 def solvePart2(data):
-    return
+    cardDict = {i: 1 for i in range(len(data))}
+
+    for i, count in zip(range(len(data)), data):
+        for _ in range(cardDict[i]):
+            for j in range(count):
+                cardDict[i + j + 1] += 1
+                
+    return sum(cardDict.values())
 
 if __name__ == '__main__':
     # read test file
@@ -14,10 +42,10 @@ if __name__ == '__main__':
     
     start = time()
     
-    part1 = solvePart1(test)
+    part1, lineData = solvePart1(test)
     p1Time = time() - start
     
-    part2 = solvePart2(test)
+    part2 = solvePart2(lineData)
     p2Time = time() - p1Time - start
     
     # print solutions and show time elapsed
